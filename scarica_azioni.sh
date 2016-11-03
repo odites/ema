96930759380dc86c5450fd6df017645675f08d9b
odites <culo@coccobellopuppa.it>
2016-11-03 21:27:30 +0100

    aggiornato per generare un file che può essere messo in append ad ema4

--- scarica_azioni.sh
+++ scarica_azioni.sh
@@ -7,7 +7,7 @@ codice_azione="PMI.MI"
 giorno_inizio="3"
 # non si capisce come mai il mese sia un numero in meno rispetto al numero vero
 mese_inizio="00"
-anno_inizio="2000"
+anno_inizio="2001"
 giorno_fine="11"
 # non si capisce come mai il mese sia un numero in meno rispetto al numero vero
 mese_fine="09"
@@ -18,9 +18,11 @@ tipo="d"
 
 wget "http://real-chart.finance.yahoo.com/table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv"
 
+mv "table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv" tabella.csv
+
 
 # Prendo la colonna cinque trasformando la virgola in spazio, poi elimino la prima riga, poi trasformo il carattere di fine linea in virgola, aggiungo un [ all'inizio, aggiungo un ] alla fine
-awk -F "\"*,\"*" '{print $5}' "table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv" | sed '1d' | sed ':a;N;$!ba;s/\n/,/g' | sed "1s/^/[/" | sed '$s/$/]/' > ${codice_azione}.txt
+awk -F "\"*,\"*" '{print $5}' tabella.csv | sed '1d' | sed 's/^/,/g' | sed "1s/^,/[/" | sed '$s/$/]/' | sed 's/^/        /g' > ${codice_azione}.txt
 
 # Cancello il csv perché è inutile
-rm "table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv"
+rm tabella.csv

baa0faab8082434f9cdd88f0efca0737b4ec03e0
odites <culo@coccobellopuppa.it>
2017-02-08 20:10:19 +0100

    script bash con più indici da fare insieme

---
+++ bombe.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Scarica da yahoo il csv di una particolare azione e poi la converte nel formato della lista di haskell
+
+
+##codice_azione="ENEL.MI"
+giorno_inizio="2"
+# non si capisce come mai il mese sia un numero in meno rispetto al numero vero
+mese_inizio="00"
+anno_inizio="2008"
+giorno_fine=$(date +%d)
+# non si capisce come mai il mese sia un numero in meno rispetto al numero vero
+mese_fine=$(date +%m)
+anno_fine=$(date +%Y)
+# d sta per giornaliero, w settimana, m mese, v solo dividendi
+tipo="d"
+
+for codice_azione in "GLD" "OIL"
+do
+
+wget "http://real-chart.finance.yahoo.com/table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv"
+#http://chart.finance.yahoo.com/table.csv?s=OIL&a=11&b=17&c=2016&d=0&e=17&f=2017&g=d&ignore=.csv
+mv "table.csv?s=${codice_azione}&a=${mese_inizio}&b=${giorno_inizio}&c=${anno_inizio}&d=${mese_fine}&e=${giorno_fine}&f=${anno_fine}&g=${tipo}&ignore=.csv" tabella.csv
+
+
+# Prendo la colonna cinque trasformando la virgola in spazio, poi elimino la prima riga, poi trasformo il carattere di fine linea in virgola, aggiungo un [ all'inizio, aggiungo un ] alla fine
+awk -F "\"*,\"*" '{print $7}' tabella.csv | sed '1d' | sed 's/^/,/g' | sed "1s/^,/[/" | sed '$s/$/]/' | sed 's/^/        /g' > ${codice_azione}.txt
+
+# Cancello il csv perché è inutile
+rm tabella.csv
+
+cat bomba.hs > tmp.hs
+cat ${codice_azione}.txt >> tmp.hs
+
+ghc tmp.hs
+./tmp > res_${codice_azione}.txt
+rm tmp*
+
+done
